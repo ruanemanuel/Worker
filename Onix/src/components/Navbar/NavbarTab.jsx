fd1ceb41125c90ab6f7eb9e76a6361f24b50f123
ruanemanuel <ruanemanueldefreitas1976@gmail.com>
2026-07-13 19:18:35 -0300

    Estruturando as páginas e Compactando o código(Parte II)

--- Onix/src/components/Navbar/NavbarTab.jsx
+++ Onix/src/components/Navbar/NavbarTab.jsx
@@ -1,13 +1,14 @@
+import {Link, NavLink} from "react-router-dom";
+import React from 'react';
 
-import React from 'react'
-
-const NavbarTab = ({title, callback}) => {
+const NavbarTab = ({title, callback, href}) => {
   return (
-    <button className="hover:bg-white/20 rounded-md p-2 pointer border-[#68696d] border-2"
+    <a className="hover:bg-white/20 rounded-md p-2 pointer border-[#68696d] border-2 text-center"
             onClick={() => callback(true)}
+            href={href}
     >
       {title}
-    </button>
+    </a>
   )
 }
 <NavbarTab />
--- Onix/src/pages/Home.jsx
+++ Onix/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='p-6'>
-      Home page
+      Início
     </div>
   )
 }
